2b4ee522d1ba217bb77843e403e19b726257bb10
Екатерина Савинкова <katy4.savinkova2004@yanedx.ru>
2023-02-28 19:14:42 +0300

    make brain-gcd game

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 [![Maintainability](https://api.codeclimate.com/v1/badges/1f631f1f6dbb0c6b89f2/maintainability)](https://codeclimate.com/github/kate-savinkova/frontend-project-44/maintainability)
 [![asciicast](https://asciinema.org/a/r9yfd4idbk8h5a4OnK2wr4Foa.svg)](https://asciinema.org/a/r9yfd4idbk8h5a4OnK2wr4Foa)
 [![asciicast](https://asciinema.org/a/aviMFj6aMPppYFUyjXHg9wIIP.svg)](https://asciinema.org/a/aviMFj6aMPppYFUyjXHg9wIIP)
+[![asciicast](https://asciinema.org/a/GnUw5ElsIfH7o4z6ckS6hCvhG.svg)](https://asciinema.org/a/GnUw5ElsIfH7o4z6ckS6hCvhG)
--- src/index.js
+++ src/index.js
@@ -12,11 +12,20 @@ export const algorithm = (typeOfGame) => {
         case 'brain-calc':
             console.log('What is the result of the expression?');
             break;
+        case 'brain-gcd':
+            console.log('Find the greatest common divisor of given numbers.');
+            break;
     }
     let correct, count = 0;
     for (let i = 0; i < 3; i++) {
-        const randomNum1 = Math.floor(Math.random() * 10) + 1;
-        const randomNum2 = Math.floor(Math.random() * 10) + 1;
+        const randomNum1 = Math.floor(Math.random() * 100) + 1;
+        const randomNum2 = Math.floor(Math.random() * 100) + 1;
+        let mi;
+        if (randomNum1 > randomNum2) {
+            mi = randomNum2;
+        } else {
+            mi = randomNum1;
+        }
         switch (typeOfGame) {
             case 'brain-even':
                 console.log(`Question: ${randomNum1}`);
@@ -41,6 +50,15 @@ export const algorithm = (typeOfGame) => {
                         console.log(`Question: ${randomNum1} * ${randomNum2}`);
                         correct = randomNum1 * randomNum2;
                         break;
+                }
+                break;
+            case 'brain-gcd':
+                console.log(`Question: ${randomNum1} ${randomNum2}`);
+                for (mi; mi >= 1; mi--) {
+                    if (randomNum1 % mi === 0 && randomNum2 % mi === 0) {
+                        correct = mi;
+                        break;
+                    }
                 }
                 break;
         }
